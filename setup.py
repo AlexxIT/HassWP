53f6564d36c1040c064daf029ac0961385773554
Alexey Khit <alexey.khit@gmail.com>
2022-05-12 10:28:22 +0300

    Update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(
-    name="Hass Win",
+    name="hass_win",
     version="0.1.0",
-    description="Windows Runner for Home Assistant",
+    description="Windows runner for Home Assistant",
     author="AlexxIT",
-    install_requires=["homeassistant"],
+    install_requires=["homeassistant", "colorlog"],
     packages=["hass_win"]
 )
